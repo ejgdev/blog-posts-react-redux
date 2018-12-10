07e5ed003444c66935d61783a5fc1f863bfcce0a
Edward Jimenez <lobox.ed@gmail.com>
2018-12-10 15:00:20 -0500

    update package

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'React-router-dom';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
